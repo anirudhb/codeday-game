c411af2a80d2fbd1a55be256e482bd8e0ba0af8b
anirudhb <anirudhqazxswced@gmail.com>
2017-11-11 14:18:10 -0800

    Anirudh

--- CodeD.py
+++ CodeD.py
@@ -1,1 +1,14 @@
-# Let's actually work on PyGame
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Super Platform")
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
